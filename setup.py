74afab4987beac718c74f9dd8db4f293757f9f8c
Aaron Halfaker <ahalfaker@wikimedia.org>
2020-01-13 21:04:22 +0000

    Convert to about.py pattern.

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import os
 from distutils.core import setup
 
 from setuptools import find_packages
+about_path = os.path.join(os.path.dirname(__file__), "mwcli/about.py")
+exec(compile(open(about_path).read(), about_path, "exec"))
 
 
 def requirements(fname):
@@ -10,16 +12,15 @@ def requirements(fname):
 
 
 setup(
-    name='mwcli',
-    version="0.0.2",
-    author='Aaron Halfaker',
-    author_email='aaron.halfaker@gmail.com',
+    name=__name__,  # noqa
+    version=__version__,  # noqa
+    author=__author__,  # noqa
+    author_email=__author_email__,  # noqa
+    description=__description__,  # noqa
+    url=__url__,  # noqa
+    license=__license__,  # noqa
     packages=find_packages(),
     scripts=[],
-    url='http://pypi.python.org/pypi/mwcli',
-    license=open('LICENSE').read(),
-    description='A set of utilities for writing MediaWiki command-line ' +
-                'utilities',
     long_description=open('README.md').read(),
     install_requires=requirements("requirements.txt"),
     test_suite='nose.collector',
